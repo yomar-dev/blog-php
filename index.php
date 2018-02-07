41bd72406afc99a30087297fc5704976fa6f5dc9
Yomar Miranda <mirandayomar@gmail.com>
2018-02-06 21:06:04 -0500

    Creamos el archivo que contiene la conexción a la BD y la página principal de nuestro Blog la cual está divida en dos secciones, una para el contenido y otro para la barra lateral y para maquetar estamos utilizando Bootstrap.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Blog</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Blog Title</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-8">
+				Content
+			</div>
+			<div class="col-md-4">
+				Sidebar		
+			</div>
+		</div>
+	</div>
+</body>
+</html>
