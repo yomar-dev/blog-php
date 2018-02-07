ef268811ea11c3427bb977396f90fbc3749811d7
Yomar Miranda <mirandayomar@gmail.com>
2018-02-06 21:26:19 -0500

    Creamos contenido estatico para ver como va quedando el Blog.

--- index.php
+++ index.php
@@ -17,12 +17,61 @@
 
 		<div class="row">
 			<div class="col-md-8">
-				Content
+				<div class="blog-post">
+					<h2>Miltlike</h2>
+					<p>
+						Jan 1, 2020 by <a href="#">Roosevelt</a>
+					</p>
+					<div class="blog-post-image">
+						<img src="http://lorempixel.com/750/250/sports/1" alt="">
+					</div>
+					<div class="blog-post-content">
+						Moio snuggle trichomatose hemadynamics asphyxiant francium Babylonite myogenetic catch grandmotherism pericystic alfa noctambulant pyramider length butylation bombacaceous Turcoman semperidentical cassiduloid lectureproof Gordonia acephaline megachiropteran award resize doweress unkist unlisty geotectonic asymbolia orchidorrhaphy mulattress rabbinism interlingual puritanically gliriform monophyleticism nasutiform cystamine polycitral stalwartism gambogic upseek underconsciousness Stenotaphrum Trochus seminaristic.
+					</div>
+				</div>
+
+				<div class="blog-post">
+					<h2>Bradburya</h2>
+					<p>
+						Jan 1, 2020 by <a href="#">Kayce</a>
+					</p>
+					<div class="blog-post-image">
+						<img src="http://lorempixel.com/750/250/sports/2" alt="">
+					</div>
+					<div class="blog-post-content">
+						Percussioner orchiorrhaphy messin sherbetlee yinst paramagnetism tadpolehood unfreezable nephology unlivableness holer historicogeographical ladder draffman incitress oecumenian bantayan hygrometrically morrowtide Mazda scallion totipotential unportrayed retinene toponymy basifier Lupercalian bracingness suspensible tetragynous dermatographia preadamitic oversale unillumination puja globefish splenophrenic lecithinase unbuttonment unfaceable Congreve staphylodermatitis supermagnificently coumalin chous praise antimycotic aplasia.
+					</div>
+				</div>
+
+				<div class="blog-post">
+					<h2>Metropolis</h2>
+					<p>
+						Jan 1, 2020 by <a href="#">Jeannette</a>
+					</p>
+					<div class="blog-post-image">
+						<img src="http://lorempixel.com/750/250/sports/3" alt="">
+					</div>
+					<div class="blog-post-content">
+						Vira dartman animadversion dermasurgery Saiva aerolith Kolis grooveless tideland Theligonum place retinoscopist illustrational hyperopia amchoor spearcast ripsaw antitypy sierran Rubicola fuji attachable sternofacialis enricher elb lastage preclean innascible oculozygomatic detailedness pachydermal transcurrent reflectional precordium fallectomy spadework barroom valvate pseudoimpartial slipcoat allegator lapidity rabbit surfactant dace unemulsified syzygium shamed.
+					</div>
+				</div>
 			</div>
 			<div class="col-md-4">
-				Sidebar		
+				<h2>Sidebar</h2>
+				<p>
+					Brachinus hydrotomy Gobiiformes parodist zonated contemplation pinonic crystallogenetic restorationer revivingly openwork microblephary dishpan unbesought bekah latro unstructural lipothymial few obolary lalling gyral misprovidence aurophore zoopantheon caulicule retrievement shipment Hockday isidoid patrondom romeite Barosma hydroclimate horizontically inverter Illuministic pocketable sixty denticulation cantative smeech forficated elementariness spinigrade washstand Ning telecode.
+				</p>
 			</div>
 		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<footer>
+					Glazing sleepland By <a href="#" title="">Kim</a>
+				</footer>
+			</div>
+		</div>
+
 	</div>
 </body>
 </html>
