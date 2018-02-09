138b082aaed82a68a116538782bc475314f8125d
Yomar Miranda <mirandayomar@gmail.com>
2018-02-08 22:42:04 -0500

    Ahora si accedemos a la raiz de la carpeta 'public' se nos mostrará el texto retornado en la función (Route /).

--- public/index.php
+++ public/index.php
@@ -4,24 +4,29 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+/**
+ * Módulo de Autoloading de composer.
+ */
+require_once '../vendor/autoload.php';
+
 require_once '../config.php';
 
 $route = $_GET['route'] ?? '/';
 
-switch ($route) {
-	case '/':
-		require '../index.php';
-		break;
-	case '/admin':
-		require '../admin/index.php';
-		break;
-	case '/admin/posts':
-		require '../admin/posts.php';
-		break;	
-	default:
-		require '../index.php';
-		break;
-}
+use Phroute\Phroute\RouteCollector;
 
+$router = new RouteCollector();
+$router->get('', function(){
+	return 'Route /';
+});
+
+/**
+ * Objeto que va a tomar la ruta que nos esta llegando para luego
+ * llamar el metodo que realmente necesita.
+ */
+$dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $route);
+echo $response;
 
 ?>
