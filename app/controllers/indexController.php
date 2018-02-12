18211ba4e85a9b269f642367b8ae48ce3a1a7726
Yomar Miranda <mirandayomar@gmail.com>
2018-02-11 21:14:59 -0500

    Dentro de nuestro 'IndexController' hemos creado una función que nos va a cargar la información de la vista principal en cuanto se llame la ruta principal de nuestro directorio 'public'. Nota: Cabe aclarar que esto aún no va a funcionar porque hay que realizar algunos cambios en  el archivo 'index' del directorio 'public'.

---
+++ app/controllers/indexController.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Controllers;
+
+class IndexController{
+
+	public function getIndex(){
+		global $pdo;
+		
+		$sql = "SELECT * FROM blog_posts ORDER BY id DESC";
+		$query = $pdo->prepare($sql);
+		$query->execute();
+
+		$blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
+		return render('../views/index.php', ['blogPosts' => $blogPosts]);
+	}
+}
+
+
+?>
