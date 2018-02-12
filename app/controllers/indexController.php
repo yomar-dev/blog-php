a97053cd1dba50d8efa183727d7351950df539f3
Yomar Miranda <mirandayomar@gmail.com>
2018-02-11 21:37:21 -0500

    Con esto estamos indicando a PHP que PDO hace parte de nuestro namespace global y no trate de buscarlo en nuestro namespace actual.

--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -6,12 +6,12 @@ class IndexController{
 
 	public function getIndex(){
 		global $pdo;
-		
+
 		$sql = "SELECT * FROM blog_posts ORDER BY id DESC";
 		$query = $pdo->prepare($sql);
 		$query->execute();
 
-		$blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
+		$blogPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
 		return render('../views/index.php', ['blogPosts' => $blogPosts]);
 	}
 }
