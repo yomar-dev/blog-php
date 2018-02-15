c5df1a46b57686de2a8a2b3f40fc394f5111aeea
Yomar Miranda <mirandayomar@gmail.com>
2018-02-14 22:39:50 -0500

    Ahora pasamos el resto de las vistas a Twig y todo sigue funcionando bien.

--- app/controllers/admin/PostController.php
+++ app/controllers/admin/PostController.php
@@ -1,8 +1,9 @@
 <?php 
 
 namespace App\Controllers\Admin;
+use App\Controllers\BaseController;
 
-class PostController{
+class PostController extends BaseController{
 
 	public function getIndex(){
 		global $pdo;
@@ -12,11 +13,11 @@ class PostController{
 		$query->execute();
 
 		$blogPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
-		return render('../views/admin/posts.php', ['blogPosts' => $blogPosts]);
+		return $this->render('admin/posts.twig', ['blogPosts' => $blogPosts]);
 	}
 
 	public function getCreate(){
-		return render('../views/admin/insert-post.php');
+		return $this->render('admin/insert-post.twig');
 	}
 
 	public function postCreate(){
@@ -29,7 +30,7 @@ class PostController{
 			'title' => $_POST['title'],
 			'content' => $_POST['content']
 		]);
-		return render('../views/admin/insert-post.php', ['result' => $result]);
+		return $this->render('admin/insert-post.twig', ['result' => $result]);
 	}
 }
 
