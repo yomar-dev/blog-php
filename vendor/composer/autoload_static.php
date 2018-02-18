4be3428cb527d51163cf8b7abf00b893fb14538f
Yomar Miranda <mirandayomar@gmail.com>
2018-02-18 11:24:34 -0500

    Hemos instalado 'siriusphp/validation mediante composer para realizar validaciones de formularios.'

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,6 +20,7 @@ class ComposerStaticInitf60f12389d352b3f5ca502c1733e5c33
         array (
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Component\\Translation\\' => 30,
+            'Sirius\\Validation\\' => 18,
         ),
         'P' => 
         array (
@@ -61,6 +62,10 @@ class ComposerStaticInitf60f12389d352b3f5ca502c1733e5c33
         'Symfony\\Component\\Translation\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/translation',
+        ),
+        'Sirius\\Validation\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/siriusphp/validation/src',
         ),
         'Psr\\SimpleCache\\' => 
         array (
