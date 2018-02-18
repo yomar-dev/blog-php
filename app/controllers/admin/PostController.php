07c521cfcca134a6c6a16874bdd294fcda0264f3
Yomar Miranda <mirandayomar@gmail.com>
2018-02-17 23:05:09 -0500

    Ahora estamos reemplazando nuestras consultas hechas manualmente por los metodos que nos ofrece PDO para consultar y guardar información.

--- app/controllers/admin/PostController.php
+++ app/controllers/admin/PostController.php
@@ -6,13 +6,8 @@ use App\Controllers\BaseController;
 class PostController extends BaseController{
 
 	public function getIndex(){
-		global $pdo;
 
-		$sql = "SELECT * FROM blog_posts ORDER BY id DESC";
-		$query = $pdo->prepare($sql);
-		$query->execute();
-
-		$blogPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
+		$blogPosts = BlogPost::all();
 		return $this->render('admin/posts.twig', ['blogPosts' => $blogPosts]);
 	}
 
@@ -21,15 +16,13 @@ class PostController extends BaseController{
 	}
 
 	public function postCreate(){
-		global $pdo;
-
-		$sql = "INSERT INTO blog_posts (title, content) VALUES (:title, :content)";
-		$query = $pdo->prepare($sql);
-		
-		$result = $query->execute([
+		$blogPost = new BlogPost([
 			'title' => $_POST['title'],
 			'content' => $_POST['content']
 		]);
+
+		$blogPost->save();		
+		$result = true;
 		return $this->render('admin/insert-post.twig', ['result' => $result]);
 	}
 }
