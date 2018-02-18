7233f341e76657783477c0ebba2c347d997f0ec7
Yomar Miranda <mirandayomar@gmail.com>
2018-02-17 23:36:52 -0500

    Base de datos actualizada con nueva información y con los nuevos campos 'created_at' y 'updated_at'.

--- cursophp.sql
+++ cursophp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-02-2018 a las 03:48:04
+-- Tiempo de generación: 18-02-2018 a las 05:34:12
 -- Versión del servidor: 10.1.28-MariaDB
 -- Versión de PHP: 7.1.10
 
@@ -31,18 +31,24 @@ SET time_zone = "+00:00";
 CREATE TABLE `blog_posts` (
   `id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
-  `content` longtext NOT NULL
+  `content` longtext NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `blog_posts`
 --
 
-INSERT INTO `blog_posts` (`id`, `title`, `content`) VALUES
-(1, 'Miltlike', 'Moio snuggle trichomatose hemadynamics asphyxiant francium Babylonite myogenetic catch grandmotherism pericystic alfa noctambulant pyramider length butylation bombacaceous Turcoman semperidentical cassiduloid lectureproof Gordonia acephaline megachiropteran award resize doweress unkist unlisty geotectonic asymbolia orchidorrhaphy mulattress rabbinism interlingual puritanically gliriform monophyleticism nasutiform cystamine polycitral stalwartism gambogic upseek underconsciousness Stenotaphrum Trochus seminaristic.'),
-(2, 'Bradburya', 'Percussioner orchiorrhaphy messin sherbetlee yinst paramagnetism tadpolehood unfreezable nephology unlivableness holer historicogeographical ladder draffman incitress oecumenian bantayan hygrometrically morrowtide Mazda scallion totipotential unportrayed retinene toponymy basifier Lupercalian bracingness suspensible tetragynous dermatographia preadamitic oversale unillumination puja globefish splenophrenic lecithinase unbuttonment unfaceable Congreve staphylodermatitis supermagnificently coumalin chous praise antimycotic aplasia.'),
-(3, 'Metropolis', 'Vira dartman animadversion dermasurgery Saiva aerolith Kolis grooveless tideland Theligonum place retinoscopist illustrational hyperopia amchoor spearcast ripsaw antitypy sierran Rubicola fuji attachable sternofacialis enricher elb lastage preclean innascible oculozygomatic detailedness pachydermal transcurrent reflectional precordium fallectomy spadework barroom valvate pseudoimpartial slipcoat allegator lapidity rabbit surfactant dace unemulsified syzygium shamed.'),
-(4, 'Lepidophyllous', 'Brachinus hydrotomy Gobiiformes parodist zonated contemplation pinonic crystallogenetic restorationer revivingly openwork microblephary dishpan unbesought bekah latro unstructural lipothymial few obolary lalling gyral misprovidence aurophore zoopantheon caulicule retrievement shipment Hockday isidoid patrondom romeite Barosma hydroclimate horizontically inverter Illuministic pocketable sixty denticulation cantative smeech forficated elementariness spinigrade washstand Ning telecode.');
+INSERT INTO `blog_posts` (`id`, `title`, `content`, `created_at`, `updated_at`) VALUES
+(1, 'Miltlike', 'Moio snuggle trichomatose hemadynamics asphyxiant francium Babylonite myogenetic catch grandmotherism pericystic alfa noctambulant pyramider length butylation bombacaceous Turcoman semperidentical cassiduloid lectureproof Gordonia acephaline megachiropteran award resize doweress unkist unlisty geotectonic asymbolia orchidorrhaphy mulattress rabbinism interlingual puritanically gliriform monophyleticism nasutiform cystamine polycitral stalwartism gambogic upseek underconsciousness Stenotaphrum Trochus seminaristic.', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'Bradburya', 'Percussioner orchiorrhaphy messin sherbetlee yinst paramagnetism tadpolehood unfreezable nephology unlivableness holer historicogeographical ladder draffman incitress oecumenian bantayan hygrometrically morrowtide Mazda scallion totipotential unportrayed retinene toponymy basifier Lupercalian bracingness suspensible tetragynous dermatographia preadamitic oversale unillumination puja globefish splenophrenic lecithinase unbuttonment unfaceable Congreve staphylodermatitis supermagnificently coumalin chous praise antimycotic aplasia.', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'Metropolis', 'Vira dartman animadversion dermasurgery Saiva aerolith Kolis grooveless tideland Theligonum place retinoscopist illustrational hyperopia amchoor spearcast ripsaw antitypy sierran Rubicola fuji attachable sternofacialis enricher elb lastage preclean innascible oculozygomatic detailedness pachydermal transcurrent reflectional precordium fallectomy spadework barroom valvate pseudoimpartial slipcoat allegator lapidity rabbit surfactant dace unemulsified syzygium shamed.', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 'Lepidophyllous', 'Brachinus hydrotomy Gobiiformes parodist zonated contemplation pinonic crystallogenetic restorationer revivingly openwork microblephary dishpan unbesought bekah latro unstructural lipothymial few obolary lalling gyral misprovidence aurophore zoopantheon caulicule retrievement shipment Hockday isidoid patrondom romeite Barosma hydroclimate horizontically inverter Illuministic pocketable sixty denticulation cantative smeech forficated elementariness spinigrade washstand Ning telecode.', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(5, 'Pantiling', 'Cabernet atmosphereless botryose closehearted Congridae belite skittle shargar levulinic recurvature functionalism inconversibility Anas obscurer Kim hyperideation underthroating mobsman Xylosma microblepharism Bletilla extradite invendibleness galipine unstrand twite eponymize Koasati gunite reinfer decompression inspoke lathing saccharated Vaishnava inhumer Euphausiacea pursuivant dracontine uncredibility bewitchful figurize suprastandard sloka pulsative rhynchonelloid Latania cicadid.', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(6, 'Unroused Paddockride', 'Bleach Phyllaurea Nici unspeckled Phyllaurea Troytown nonnucleated ambilogy iatrophysics Thackerayesque determinant Baptornis Petrinism monomolecular lugworm scarred sixte nongerundial commixture lymphocytotoxin parabulic nyctipithecine barlock heptarchic yeomaness extund pidan imbecility selly asserter uncontradictably vection jangly unsuppled fitcher bocaccio rodwood uller undercloth hydroscopical genthite undoffed titleproof marcgraviaceous myringotome kamichi columnarity mistendency.', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(7, 'Sporogonic', 'Crannage pharmaceutically Spalax oversourness eunuch otherwiseness monkeylike stereomonoscope Piscis gutturalism Hessian chrysoberyl unsymbolical teste spermatiogenous diphthongal unfenced pegbox tornade skart selensilver nonabstainer implausibly interferer.', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(8, 'Lassiehood', 'Unrealistic waysliding malacostracology ethnogeographic complanar bunghole agraphia Papaveraceae antinational hairwork minacious sulpholipin neoparaffin reccy pentahexahedron Amygdalaceae repersuasion epicycle mothersome trinitroxylene weaselwise spoollike Anaryan centuriation.', '2018-02-18 05:16:57', '2018-02-18 05:16:57');
 
 -- --------------------------------------------------------
 
@@ -91,7 +97,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `blog_posts`
 --
 ALTER TABLE `blog_posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
