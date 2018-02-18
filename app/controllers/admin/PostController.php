ba0e2b5f7c55a54e6aa044d6cd9f5c4e674a1bb8
Yomar Miranda <mirandayomar@gmail.com>
2018-02-18 11:37:27 -0500

    Ahora realizamos las respectivas validaciones del lado del servidor utilizando siriusphp/validator, en este caso solo estamos indicando que el campo 'title' y 'content' deben ser requeridos.

--- app/controllers/admin/PostController.php
+++ app/controllers/admin/PostController.php
@@ -3,11 +3,11 @@
 namespace App\Controllers\Admin;
 use App\Controllers\BaseController;
 use App\Models\BlogPost;
+use Sirius\Validation\Validator;
 
 class PostController extends BaseController{
 
 	public function getIndex(){
-
 		$blogPosts = BlogPost::all();
 		return $this->render('admin/posts.twig', ['blogPosts' => $blogPosts]);
 	}
@@ -17,14 +17,34 @@ class PostController extends BaseController{
 	}
 
 	public function postCreate(){
-		$blogPost = new BlogPost([
-			'title' => $_POST['title'],
-			'content' => $_POST['content']
-		]);
+		$errors = [];
+		$result = false;
+		$validator = new Validator();
+		/**
+		 * Indicamos que el campo 'title' y 'content' son requeridos.
+		 */
+		$validator->add('title', 'required');
+		$validator->add('content', 'required');
 
-		$blogPost->save();		
-		$result = true;
-		return $this->render('admin/insert-post.twig', ['result' => $result]);
+		if($validator->validate($_POST)){			
+			$blogPost = new BlogPost([
+				'title' => $_POST['title'],
+				'content' => $_POST['content']
+			]);
+
+			$blogPost->save();		
+			$result = true;
+		}else{
+			/**
+			 * Obtenemos todos los errores devueltos por el validador.
+			 */
+			$errors = $validator->getMessages();
+		}
+
+		return $this->render('admin/insert-post.twig', [
+			'result' => $result,
+			'errors' => $errors
+		]);
 	}
 }
 
